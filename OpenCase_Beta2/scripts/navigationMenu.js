2164294f7ba7c518fa1e2a5e5ea7234f6360a9e0
VLADOS776 <kurtukovvlad@gmail.com>
2017-06-13 23:21:34 +0300

    Yandex Metrica

--- OpenCase_Beta2/scripts/navigationMenu.js
+++ OpenCase_Beta2/scripts/navigationMenu.js
@@ -163,4 +163,9 @@ $(function () {
     $(document).on('doublechanged', function() {
         $('#menu_doubleBalance').text(getStatistic('doubleBalance'));
     })
+    
+    // Yandex Metrika
+    var code = '<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter44978944 = new Ya.Metrika({ id:44978944, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, ut:"noindex" }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/44978944?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->';
+    
+    $(document.body).append(code);
 })
